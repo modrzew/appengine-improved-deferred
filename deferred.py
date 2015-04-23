ba819dbbcf058fa9d540a41901beb919a4ff00c8
Modrzew <me@modriv.net>
2015-04-23 21:09:06 +0200

    Docstrings, _generate_name

--- deferred.py
+++ deferred.py
@@ -3,6 +3,7 @@ Improved deferred library for App Engine
 
 some description here
 """
+import logging
 import time
 
 from google.appengine.api import taskqueue
@@ -15,6 +16,17 @@ _MAX_RETRIES = 5
 
 
 def _execute(executor, *args, **kwargs):
+    """Puts task function on the queue using provided executor
+
+    Executor may be either deferred.defer or taskqueue.add.
+    In case of DeadlineExceededError, it does its best to retry deferring the
+    task. And taskqueue can be randomly unavailable for short spans, or RPC
+    can return after too long time (and sadly, there's no way to change its
+    deadline).
+    It also catches two exceptions that may be raised by taskqueue:
+    - TaskAlreadyExistsError (in case task is alreaady present on the queue),
+    - TombstonedTaskError (in case task was already executed).
+    """
     times_tried = 0
     while True:
         try:
@@ -22,18 +34,27 @@ def _execute(executor, *args, **kwargs):
         except DeadlineExceededError:
             if times_tried >= _MAX_RETRIES:
                 raise
-            time.sleep(1)  # I know what I'm doing, trust me
+            time.sleep(0.5 * times_tried)  # I know what I'm doing, trust me
             continue
         except taskqueue.TaskAlreadyExistsError:
-            pass
+            logging.warning('Task already exists!')
         except taskqueue.TombstonedTaskError:
-            pass
+            logging.warning('Task tombstoned!')
         break
 
 
-def defer(*args, **kwargs):
+def _generate_name(args, kwargs):
+    """Generates unique name for given args/kwargs"""
+    return 'unique-name'  # will suffice for now
+
+
+def defer(func_or_path, *args, **kwargs):
+    """Defers the task in an intelligent way"""
+    if '_name' not in kwargs:
+        kwargs['_name'] = _generate_name(args, kwargs)
     _execute(
         gae_deferred.defer,
+        func_or_path,
         *args,
         **kwargs
     )
