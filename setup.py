7340cf4e4cfae2ae76806b4ba6f6c5d094ea4ecc
Modrzew <me@modriv.net>
2015-05-03 16:18:50 +0200

    setup.py pseudostash

---
+++ setup.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+"""
+blah
+"""
+from setuptools import setup
+
+
+install_requires = [
+]
+
+tests_require = [
+    'pytest',
+    'mock',
+]
+
+
+setup(
+    name='appengine-improved-deferred',
+    version='0.1',
+    description='blah',
+    author='Tomasz Modrzyński',
+    # packages=['src'],
+    install_requires=install_requires,
+    tests_require=tests_require,
+)
