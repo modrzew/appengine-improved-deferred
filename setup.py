580a1d2b076a943d03064ada67668d78a3c14158
Modrzew <dev@modriv.net>
2015-05-10 22:41:22 +0200

    tox power
    setup.py shrinked in size - all tests requirements are now in tox.ini.
    Travis should also utilize it to run tests.

--- setup.py
+++ setup.py
@@ -5,21 +5,11 @@ blah
 from setuptools import setup
 
 
-install_requires = [
-]
-
-tests_require = [
-    'pytest',
-    'mock',
-]
-
-
 setup(
     name='appengine-improved-deferred',
     version='0.1',
     description='blah',
     author='Tomasz Modrzyński',
-    # packages=['src'],
-    install_requires=install_requires,
-    tests_require=tests_require,
+    py_modules=['deferred'],
+    install_requires=[],
 )
