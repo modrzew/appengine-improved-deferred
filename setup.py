0a7d4e11915eb60d69198753239677f475d67d25
Modrzew <dev@modriv.net>
2015-05-11 13:20:44 +0200

    coveralls, y u not run?

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 # coding: utf-8
 """
-blah
+Improved deferred library for Google App Engine
+
+It's an adapter between deferred.defer and taskqueue.
+For more info and usage, see README file.
 """
 from setuptools import setup
 
 
 setup(
     name='appengine-improved-deferred',
-    version='0.1',
-    description='blah',
+    version='1.0',
+    description='Improved deferred library for Google App Engine',
     author='Tomasz Modrzyński',
     py_modules=['deferred'],
     install_requires=[],
